7bde703ab0213933f4964018fd2d4cd83a3c3fd1
randhir885 <69856474+randhir885@users.noreply.github.com>
2026-06-28 18:25:43 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,10 @@
+// Import Supabase
+const SUPABASE_URL = "https://ulpqarobuynqopwzuezt.supabase.co";
 
+const SUPABASE_ANON_KEY = "sb_publishable_XcNLsPe2u4iDOI4iBjF72w_Qlo_iVBG";
+
+// Create Supabase Client
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
